3aab4130c55616dfc4e7387d470fd9d556409a64
hamza fikri <hamzawin2018@gmail.com>
2025-10-23 10:48:42 +0200

    initializing project in gitlab

---
+++ tarnished/project-model/src/main/java/fr/uha/ensisa/gl/tarnished/App.java
@@ -0,0 +1,13 @@
+package fr.uha.ensisa.gl.tarnished;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
