269eedaf4c85b2f23f4472b4f92d87eed8f23fbe
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-11-11 23:20:58 +0100

    Update create-column: push current workspace

---
+++ tarnished/project-model/src/main/java/fr/uha/ensisa/gl/entities/Column.java
@@ -0,0 +1,28 @@
+package fr.uha.ensisa.gl.entities;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+import java.util.List;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Column {
+
+    private Long id;
+
+    private String name;
+
+    private int position;
+
+    private int maxCapacity;
+
+    private Project project;
+
+    private List<Story> stories;
+
+}
