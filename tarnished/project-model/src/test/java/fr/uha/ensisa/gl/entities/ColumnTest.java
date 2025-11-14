6f5ba53ee2e294211ede587e111c6918ffe3f498
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-11-14 12:39:04 +0100

    #53 add mockito test to column class

---
+++ tarnished/project-model/src/test/java/fr/uha/ensisa/gl/entities/ColumnTest.java
@@ -0,0 +1,42 @@
+package fr.uha.ensisa.gl.entities;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ColumnTest {
+
+    @Mock
+    Project project;
+
+    @Mock
+    Story story1;
+
+    @Mock
+    Story story2;
+
+    @Test
+    void columnGettersAndSettersWork() {
+        Column c = new Column();
+        c.setId(123);
+        c.setName("col");
+        c.setPosition(1);
+        c.setMaxCapacity(50);
+        c.setProject(project);
+        List<Story> stories = List.of(story1, story2);
+        c.setStories(stories);
+
+        assertEquals(123, c.getId());
+        assertEquals("col", c.getName());
+        assertEquals(1, c.getPosition());
+        assertEquals(50, c.getMaxCapacity());
+        assertSame(project, c.getProject());
+        assertEquals(stories, c.getStories());
+    }
+}
