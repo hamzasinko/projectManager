5919724a7764a8b36a9594bb1cb5d80808c9ca25
hytlot <hayatlotfi2023@gmail.com>
2025-11-16 16:41:36 +0100

    Ajout de la classe Tag et des tests associés, nettoyage .idea

--- tarnished/project-model/src/main/java/fr/uha/ensisa/gl/entities/Tag.java
+++ tarnished/project-model/src/main/java/fr/uha/ensisa/gl/entities/Tag.java
@@ -6,8 +6,8 @@ import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class tag {
-    private Long id;
+public class Tag {
+    private int id;
     private String name;
     private String color;
     private List<String> categories;
