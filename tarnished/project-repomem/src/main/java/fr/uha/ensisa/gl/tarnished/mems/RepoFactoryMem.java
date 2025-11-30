8e67e043541df2eccf9d9562f7ab04163a15f27d
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-11-30 13:19:37 +0100

    #78: Update RepoFactory and RepoFactoryMem

--- tarnished/project-repomem/src/main/java/fr/uha/ensisa/gl/tarnished/mems/RepoFactoryMem.java
+++ tarnished/project-repomem/src/main/java/fr/uha/ensisa/gl/tarnished/mems/RepoFactoryMem.java
@@ -2,6 +2,7 @@ package fr.uha.ensisa.gl.tarnished.mems;
 
 import fr.uha.ensisa.gl.tarnished.repos.ColumnRepo;
 import fr.uha.ensisa.gl.tarnished.repos.ProjectRepo;
+import fr.uha.ensisa.gl.tarnished.repos.StoryRepo;
 import fr.uha.ensisa.gl.tarnished.repos.RepoFactory;
 import org.springframework.stereotype.Component;
 
@@ -9,6 +10,7 @@ import org.springframework.stereotype.Component;
 public class RepoFactoryMem implements RepoFactory {
     public final ColumnRepo columnRepo = new ColumnRepoMem();
     public final ProjectRepo projectRepo = new ProjectRepoMem();
+    public final StoryRepo storyRepo = new StoryRepoMem();
 
     @Override
     public ColumnRepo getColumnRepo() {
@@ -18,5 +20,10 @@ public class RepoFactoryMem implements RepoFactory {
     @Override
     public ProjectRepo getProjectRepo() {
         return this.projectRepo;
+    }
+
+    @Override
+    public StoryRepo getStoryRepo() {
+        return this.storyRepo;
     }
 }
