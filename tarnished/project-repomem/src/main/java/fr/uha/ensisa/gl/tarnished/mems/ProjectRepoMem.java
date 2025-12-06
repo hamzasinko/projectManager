9b18aeafd70a01280d518791e787f29cee860ae3
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-12-06 22:22:30 +0100

    #83 Add column name editing and display IN PROGRESS with spaces

--- tarnished/project-repomem/src/main/java/fr/uha/ensisa/gl/tarnished/mems/ProjectRepoMem.java
+++ tarnished/project-repomem/src/main/java/fr/uha/ensisa/gl/tarnished/mems/ProjectRepoMem.java
@@ -1,7 +1,10 @@
 package fr.uha.ensisa.gl.tarnished.mems;
 
+import fr.uha.ensisa.gl.entities.Column;
 import fr.uha.ensisa.gl.entities.Project;
+import fr.uha.ensisa.gl.tarnished.repos.ColumnRepo;
 import fr.uha.ensisa.gl.tarnished.repos.ProjectRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
@@ -11,12 +14,35 @@ public class ProjectRepoMem implements ProjectRepo {
     private final Map<Long, Project> store =
             Collections.synchronizedMap(new TreeMap<Long, Project>());
     private int nextId = 1;
+    
+    @Autowired
+    private ColumnRepo columnRepo;
+    
+    public void setColumnRepo(ColumnRepo columnRepo) {
+        this.columnRepo = columnRepo;
+    }
 
     @Override
     public void persist(Project project) {
         project.setId(nextId++);
         project.setDateStarted(Calendar.getInstance().getTime());
         store.put((long)project.getId(), project);
+        
+        // Create default columns
+        createDefaultColumns(project);
+    }
+    
+    private void createDefaultColumns(Project project) {
+        String[] defaultColumns = {"TODO", "IN PROGRESS", "REVIEW", "DONE", "BLOCKED"};
+        
+        for (int i = 0; i < defaultColumns.length; i++) {
+            Column column = new Column();
+            column.setName(defaultColumns[i]);
+            column.setProject(project);
+            column.setPosition(i + 1);
+            column.setMaxCapacity(0); // No limit by default
+            columnRepo.persist(column);
+        }
     }
 
     @Override
