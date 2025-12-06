266d4387702ed431ac3f0b5376a0a62e1e01f324
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-12-06 11:25:43 +0100

    #75: Add timer endpoints in StoryController and worklog tests

--- tarnished/project-model/src/main/java/fr/uha/ensisa/gl/entities/WorkLog.java
+++ tarnished/project-model/src/main/java/fr/uha/ensisa/gl/entities/WorkLog.java
@@ -3,16 +3,39 @@ package fr.uha.ensisa.gl.entities;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class WorkLog {
-    private int id;
-    private float duration;
-    private Date hourStart;
-    private Date hourEnd;
-    private User user;
+    private long id;
+    private LocalDateTime start;
+    private LocalDateTime end;
+    private long duration;
+    private long userId;
+    private String comment;
+    private long storyId;
+
+    public WorkLog(long id, LocalDateTime start, long userId, long storyId) {
+        this.id = id;
+        this.start = start;
+        this.userId = userId;
+        this.storyId = storyId;
+        this.duration = 0;
+    }
+
+    public void stopTimer() {
+        if (this.end == null) {
+            this.end = LocalDateTime.now();
+            if (this.start != null) {
+                this.duration = ChronoUnit.MINUTES.between(this.start, this.end);
+            }
+        }
+    }
+
+    public boolean isRunning() {
+        return this.end == null;
+    }
 }
