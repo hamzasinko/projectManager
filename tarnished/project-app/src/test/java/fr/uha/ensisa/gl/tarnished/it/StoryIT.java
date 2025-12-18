b2e8a023a7e67203d7b07efc719aed0481fe4963
Zakariae El Bouzidi <zakariae.elbouzidi@example.com>
2025-12-18 22:15:21 +0100

    Fix port and adjust columns #107

--- tarnished/project-app/src/test/java/fr/uha/ensisa/gl/tarnished/it/StoryIT.java
+++ tarnished/project-app/src/test/java/fr/uha/ensisa/gl/tarnished/it/StoryIT.java
@@ -24,7 +24,7 @@ public class StoryIT {
         if (driver != null) return;
 
         host = System.getProperty("host", "localhost");
-        port = System.getProperty("servlet.port", "8090");
+        port = System.getProperty("servlet.port", "8080");
 
         driver = WebDriverFactory.createChromeDriver();
     }
